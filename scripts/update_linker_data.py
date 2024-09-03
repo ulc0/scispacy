71a891cacad71e294890d70a2e613195e9b154d2
ulc0 <ulc0>
2024-09-03 22:13:09 +0000

    entity linker needs its own config

--- scripts/update_linker_data.py
+++ scripts/update_linker_data.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
-# MAGIC %md
-# MAGIC freeze
+# MAGIC %pip freeze
+# MAGIC %pip install -r /Volumes/edav_dev_cdh_test/dev_cdh_ml_test/compute/requirements_scispacy_support.txt
 # MAGIC %pip install scipy==1.10.1
 # MAGIC %restart_python
 
@@ -16,9 +16,8 @@
 # COMMAND ----------
 
 # MAGIC %sh
-# MAGIC #python3 export_umls_json.py --meta_path /Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/2024AA/META --output_path /Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/jsonl/umls_kb.jsonl
 # MAGIC python3 export_umls_json.py --meta_path /Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/2024AA/META --output_path /Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/jsonl/umls_kb.jsonl
-# MAGIC python3 export_rxnorm_json.py --meta_pat/Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/rxnorm/rrf/ --output_path /Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/jsonl/rxnorm_kb.jsonl
+# MAGIC #python3 export_umls_json.py --meta_path /Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/rxnorm/rrf/ --output_path /Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/jsonl/rxnorm_kb.jsonl
 
 # COMMAND ----------
 
@@ -27,11 +26,33 @@
 
 # COMMAND ----------
 
+"""
+proformaLinkerPaths = {
+    ann_index="https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/data/linkers/2023-04-23/umls/nmslib_index.bin",  # noqa
+    tfidf_vectorizer="https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/data/linkers/2023-04-23/umls/tfidf_vectorizer.joblib",  # noqa
+    tfidf_vectors="https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/data/linkers/2023-04-23/umls/tfidf_vectors_sparse.npz",  # noqa
+    concept_aliases_list="https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/data/linkers/2023-04-23/umls/concept_aliases.json",  # noqa
+}
+"""
+
+# COMMAND ----------
+
+out_dir='/Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/'
+
+# COMMAND ----------
+
 
 from scispacy.linking_utils import KnowledgeBase
-kb_path='/Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/jsonl/umls_kb.jsonl'
-kb_path='/dbfs/kb/jsonl/umls_kb.jsonl'
+from scispacy.candidate_generation import CandidateGenerator, create_tfidf_ann_index
+from scispacy.linking import EntityLinker
+from scispacy.umls_utils import UmlsKnowledgeBase
+from scispacy.abbreviation import AbbreviationDetector
 
+#import scispacy.linking_utils as linking_utils 
+#import scispacy.candidate_generation as candidate_generation
+#import scispacy.umls_utils as umls_utils
+#import scispacy.linking as linking
+kb_path=f"{out_dir}jsonl/umls_kb.jsonl"
 kb = KnowledgeBase(file_path=kb_path)
 
 # COMMAND ----------
@@ -39,17 +60,22 @@ kb = KnowledgeBase(file_path=kb_path)
 import tempfile
 from scispacy.candidate_generation import CandidateGenerator, create_tfidf_ann_index, MentionCandidate
 import tempfile
-
+ 
 temp_dir = tempfile.mkdtemp() #.TemporaryDirectory()
 print(temp_dir)
 # use temp_dir, and when done:
-#temp_dir.cleanup()
-#with tempfile.TemporaryDirectory() as dir_name:
 umls_concept_aliases, tfidf_vectorizer, ann_index = create_tfidf_ann_index(temp_dir, kb)
-#dbutils.fs.ls(temp_dir)
-#dbutils.fs.cp(temp_dir+'/concept_aliases.json','/Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/' )
-#dbutils.fs.cp(temp_dir+'/tfidf_vectors_sparse.npz','/Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/' )
 
+
+
+# COMMAND ----------
+
+dbutils.fs.ls(temp_dir)
+dbutils.fs.cp(temp_dir+'/concept_aliases.json',out_dir)
+dbutils.fs.cp(temp_dir+'/tfidf_vectors_sparse.npz',out_dir )
+dbutils.fs.cp(temp_dir+'/tfidf_vectorizer.joblib',out_dir )
+dbutils.fs.cp(temp_dir+'/nmslib_index.bin',out_dir )
+dbutils.fs.ls(out_dir)
 
 # COMMAND ----------
 
@@ -62,7 +88,7 @@ umls_concept_aliases, tfidf_vectorizer, ann_index = create_tfidf_ann_index(temp_
 
 # COMMAND ----------
 
-# MAGIC %sh
+# MAGIC %md
 # MAGIC cd /tmp/tmpazmsqksw
 # MAGIC cp concept_aliases.json /Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/
 # MAGIC cp tfidf_vectors_sparse.npz /Volumes/edav_dev_cdh_test/dev_cdh_ml_test/data/
